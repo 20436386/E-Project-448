2e872d45f4c9f24761fdc13566166f13f4f92464
20436386 <brandon16scott@gmail.com>
2021-08-20 13:16:44 +0200

    VCP now works on windows

--- stm32-project-files/Core/Src/main.c
+++ stm32-project-files/Core/Src/main.c
@@ -100,8 +100,9 @@ int main(void)
 	  char buf[] = "hello";
 	  CDC_Transmit_FS((uint8_t*)buf, strlen(buf));
 	  HAL_Delay(1000);
-    /* USER CODE END WHILE */
 
+	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_1);
+    /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
@@ -158,10 +159,22 @@ void SystemClock_Config(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
