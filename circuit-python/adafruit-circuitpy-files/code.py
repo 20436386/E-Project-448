156defadc22924ff545136c6c2ec0b8707a40dcf
20436386@sun.ac.za <brandon16scott@gmail.com>
2021-09-20 15:16:13 +0200

    Circuitpy: Algorithm for sail control system implemented (first version)

--- circuit-python/adafruit-circuitpy-files/code.py
+++ circuit-python/adafruit-circuitpy-files/code.py
@@ -97,8 +97,9 @@ led.direction = digitalio.Direction.OUTPUT
 # gps_ident = bytes("$GNRMC", 'utf-16')
 
 
-# #set up pwm for servos
-# servo1 = pwmio.PWMOut(board.A2, frequency = 50, duty_cycle = (int)((75/1000)*(2**16)))
+#set up pwm for servos
+# rudder_servo = pwmio.PWMOut(board.A2, frequency = 50, duty_cycle = (int)((75/1000)*(2**16)))
+sail_servo = pwmio.PWMOut(board.D9, frequency = 50, duty_cycle = (int)((72/1000)*(2**16)))
 
 #Init ADC for wind sensor
 adc = analogio.AnalogIn(board.A3)
@@ -149,10 +150,32 @@ adc = analogio.AnalogIn(board.A3)
 #             file.write(bytes(str(sensor._akm._offset) , 'utf-16'))
 #             file.write(bytes(str(sensor._akm._scale) , 'utf-16'))
 
+PWM_max = 120
+PWM_min = 72
+ADC_min = 0.22
+ADC_max = 0.96
+angle_no_sail = 45
 
 while True:
 
-    print((adc.value/2**16) * 3.3)
+    print("adc_percentage = ",  (adc.value/2**16) * 100)
+    alpha = ( ( (adc.value/2**16) -ADC_min )/(ADC_max-ADC_min) )
+    print("alpha = ", alpha * 100)
+    wind_bearing = ( alpha * 360)
+    if (wind_bearing > 180):
+        wind_bearing -= 360
+    # print("wind bearing = ", wind_bearing, "\n")
+    if abs(wind_bearing) > 45:
+        print("wind bearing = ", wind_bearing)
+        PWM_val = ( (abs(wind_bearing) - angle_no_sail)/(180 - angle_no_sail) ) * (PWM_max - PWM_min) + PWM_min
+        print("PWM_val = ", PWM_val, "\n")
+        sail_servo.duty_cycle = (int)((PWM_val/1000)*(2**16))
+    else:
+        print("no-sail-zone, wind_bearing = ", wind_bearing)
+        print((sail_servo.duty_cycle/2**16)*1000)
+
+    # print("adc.value = ", (adc.value/2**16))
+    # print("voltage = ", (adc.value/2**16) * 3.3, "\n")
     time.sleep(1)
 
 
